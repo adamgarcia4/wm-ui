7bfe6b0496ef4b2ed3c516f9ff384513fe76a235
Micael Jorge <micael@micaels-mbp.mynet>
2022-05-11 15:34:54 +0300

    add some controls

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,23 +2,35 @@ import React from "react"
 import { Button } from "./Button"
 
 export default {
-  title: 'Button',
+  title: "Button",
   component: Button,
+  argTypes: {
+    disabled: {
+      control: { type: "boolean" },
+    },
+    styleName: {
+      control: {
+        type: "select",
+        options: [
+          "primary",
+          "secondary",
+          "valid",
+          "warning",
+          "alert",
+          "primary-light",
+          "light",
+          "secondary-light",
+          "valid-light",
+          "warning-light",
+          "alert-light",
+          "bid",
+          "offer",
+          "dark",
+          "best",
+        ],
+      },
+    },
+  },
 }
 
-export const Primary = () => <Button styleName="primary">Button</Button>;
-export const Secondary = () => <Button styleName="secondary">Button</Button>;
-export const Valid = () => <Button styleName="valid">Button</Button>;
-export const Warning = () => <Button styleName="warning">Button</Button>;
-export const Alert = () => <Button styleName="alert">Button</Button>;
-export const Light = () => <Button styleName="light">Button</Button>;
-export const PrimaryLight = () => <Button styleName="primary-light">Button</Button>;
-export const SecondaryLight = () => <Button styleName="secondary-light">Button</Button>;
-export const ValidLight = () => <Button styleName="valid-light">Button</Button>;
-export const WarningLight = () => <Button styleName="warning-light">Button</Button>;
-export const AlertLight = () => <Button styleName="alert-light">Button</Button>;
-export const Bid = () => <Button styleName="bid">Button</Button>;
-export const Offer = () => <Button styleName="offer">Button</Button>;
-export const Dark = () => <Button styleName="dark">Button</Button>;
-export const Best = () => <Button styleName="best">Button</Button>;
-export const Disabled = () => <Button disabled>Button</Button>;
+export const Basic = ({ ...props }) => <Button {...props}>Button</Button>
