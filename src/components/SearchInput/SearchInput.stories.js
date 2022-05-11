4f213d23def2acbb8c476de520c51c899c97e43e
barak <barak.saidoff@gmail.com>
2022-05-11 15:27:44 +0300

    main styles

---
+++ src/components/SearchInput/SearchInput.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import SearchInput from "./SearchInput"
+
+export default {
+  title: 'SearchInput',
+  component: SearchInput,
+}
+
+export const SearchInputStory = () => <div><SearchInput>Panel Section</SearchInput></div>;
